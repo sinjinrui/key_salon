9704374d91ecd8e0e7653424cce60886545110bc
sinjinrui <66107113+sinjinrui@users.noreply.github.com>
2020-10-03 21:48:05 +0900

    first_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:twitter]
+         :recoverable, :rememberable, :omniauthable, omniauth_providers: [:twitter]
+  #:validatable,を抜いたらアドレス無しで通る。
+
+  password_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i
+
+  with_options presence: true do
+    validates :name
+    validates :nickname
+    # validates :password, format: { with: password_REGEX }
+  end
+
+  validates :email, uniqueness: true
+  
 
   has_many :sns_credentials
 
@@ -13,13 +25,14 @@ class User < ApplicationRecord
       name: auth.info.name,
       nickname: auth.info.nickname,
       email: auth.info.email,
-      image: auth.info.image
+      image: auth.info.image,
+      
     )
 
     if user.persisted?
       sns.user = user
       sns.save
     end
-    user
+    { user: user, sns: sns }
   end
 end
